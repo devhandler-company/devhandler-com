beda387532e1772020cde240c43085b3ab22c7dc
Yevhen Slyva <yevhen.slyva@devhandler.com>
2025-02-22 01:03:01 +0200

    Complete about page

--- blocks/clutch/clutch.js
+++ blocks/clutch/clutch.js
@@ -1,5 +1,8 @@
 export default function decorate(block) {
-  block.innerHTML = '<button class="button"><div class="clutch-widget" data-url="https://widget.clutch.co" data-widget-type="2" data-width="280" data-height="45" data-nofollow="true" data-expandifr="true" data-clutchcompany-id="2366887"></div></button>';
+  const clutchVersion = block.classList.contains('mini')
+    ? '<div class="clutch-widget" data-url="https://widget.clutch.co" data-widget-type="1" data-height="40" data-nofollow="true" data-expandifr="true" data-clutchcompany-id="2366887"></div>'
+    : '<div class="clutch-widget" data-url="https://widget.clutch.co" data-widget-type="2" data-height="45" data-nofollow="true" data-expandifr="true" data-clutchcompany-id="2366887"></div>';
+  block.innerHTML = `<button class="button">${clutchVersion}</button>`;
   block.classList.add('button');
   block.classList.add('to-left');
 }
