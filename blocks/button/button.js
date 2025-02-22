7fcbe3a9af06e6a71c18583026cbe31f5737474c
Yevhen Slyva <yevhen.slyva@devhandler.com>
2025-02-22 23:03:33 +0200

    Finish contact us page

--- blocks/button/button.js
+++ blocks/button/button.js
@@ -1,28 +1,21 @@
-import { createTag } from "../../scripts/utils.js";
-
 export default function decorate(block) {
-  const link = block.querySelector("a");
-  if (!link) {
-    return;
-  }
+    const link = block.querySelector('a');
+    if (!link) {
+        return;
+    }
 
-  const href = link.getAttribute("href");
-  if (href && href.startsWith("#")) {
-      link.addEventListener("click", (event) => {
-          event.preventDefault();
-          const scrollToElementId = href.replace("#", "");
-          const scrollToElement = document.getElementById(scrollToElementId);
-          if (!scrollToElement) {
-              return;
-          }
-          scrollToElement.scrollIntoView({
-              behavior: "smooth",
-          });
-      });
-      return;
-  }
-
-  if (!href || !href.startsWith("#")) {
-      return;
-  }
+    const href = link.getAttribute('href');
+    if (href && href.startsWith('#')) {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            const scrollToElementId = href.replace('#', '');
+            const scrollToElement = document.getElementById(scrollToElementId);
+            if (!scrollToElement) {
+                return;
+            }
+            scrollToElement.scrollIntoView({
+                behavior: 'smooth',
+            });
+        });
+    }
 }
