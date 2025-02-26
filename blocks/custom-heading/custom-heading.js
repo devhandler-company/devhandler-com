486a4c1fac869e07498663909e0f1058c183c5d1
yevhen-slyva-devhandler <yevhen.slyva@devhandler.com>
2025-02-26 18:19:58 +0200

    Fix typed text

--- blocks/custom-heading/custom-heading.js
+++ blocks/custom-heading/custom-heading.js
@@ -1,13 +1,27 @@
 import { createTag, getBlockProperties } from '../../scripts/utils.js';
 
 const processTypedBlock = (element) => {
+  const containerPlaceholder = document.createElement('div');
+  containerPlaceholder.classList.add('typed-text-placeholder');
+  containerPlaceholder.innerHTML = element.innerText;
   const text = element.innerText;
   element.innerText = '';
+
   let charIndex = 0;
+
+  const typedTextContainer = document.createElement('div');
+  typedTextContainer.classList.add('typed-text-typing');
+  const innerContainer = document.createElement('div');
+
+  innerContainer.classList.add('typed-text-inner-container');
+  innerContainer.appendChild(containerPlaceholder);
+  innerContainer.appendChild(typedTextContainer);
+
+  element.appendChild(innerContainer);
 
   function typeWriter() {
     if (charIndex < text.length) {
-      element.innerHTML += text.charAt(charIndex);
+      typedTextContainer.innerHTML += text.charAt(charIndex);
       charIndex += 1;
       setTimeout(typeWriter, 50);
     }
