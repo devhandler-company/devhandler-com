7faf6e3a764fc3f7d44980fb809c6e97e0e205b2
mmmihhaeel <mikael11032005@gmail.com>
2025-08-08 14:26:49 +0300

    feat: fixed code snippet and added parallax effect to banner

--- blocks/blog-hero/blog-hero.js
+++ blocks/blog-hero/blog-hero.js
@@ -3,9 +3,9 @@ import { loadFragment } from '../fragment/fragment.js';
 export default async function decorate(block) {
   const picture = block.querySelector('picture');
   const texts = block.querySelectorAll('p');
-  const titleText = texts[0].innerText;
-  const descriptionText = texts[1]?.innerText;
-  const link = texts[2]?.querySelector('a');
+  const titleText = texts?.[0]?.innerText;
+  const descriptionText = texts?.[1]?.innerText;
+  const link = texts?.[2]?.querySelector('a');
 
   block.innerText = '';
   block.appendChild(picture);
@@ -20,7 +20,7 @@ export default async function decorate(block) {
   if (descriptionText || link) {
     heading.classList.add('blog-hero-text-short');
   }
-  heading.innerText = titleText;
+  heading.innerText = titleText || '';
   heroContent.appendChild(heading);
 
   if (descriptionText) {
@@ -36,9 +36,57 @@ export default async function decorate(block) {
   heroContent.appendChild(heroOptionalContainer);
 
   if (block.classList.contains('search')) {
-    const searchFragment = await loadFragment('/fragments/search-blog');
+    const searchFragment = loadFragment('/fragments/search-blog');
     heroContent.appendChild(searchFragment?.firstElementChild?.firstElementChild);
   }
 
   block.appendChild(heroContent);
+
+  if (block.classList.contains('parallax')) {
+    const imgEl = picture.querySelector('img');
+    const url = imgEl?.currentSrc || imgEl?.src;
+    if (url) {
+      block.style.backgroundImage = `url("${url}")`;
+      block.style.backgroundSize = 'cover';
+      block.style.backgroundPosition = 'top';
+      block.style.backgroundAttachment = 'fixed';
+      block.style.backgroundRepeat = 'no-repeat';
+
+      const setAspectRatioOnResize = () => {
+        let w = imgEl.width;
+        let h = imgEl.height;
+        if ((!w || !h) && imgEl.hasAttribute('width') && imgEl.hasAttribute('height')) {
+          w = parseInt(w, 10);
+          h = parseInt(h, 10);
+        }
+        if (window.innerWidth > 1024) {
+          block.style.backgroundAttachment = 'fixed';
+        }
+        if (window.innerWidth <= 1024) {
+          block.style.backgroundAttachment = 'scroll';
+        }
+        if (w > 0 && h > 0 && CSS.supports('aspect-ratio', '1/1')) {
+          block.style.aspectRatio = `${w} / ${h}`;
+          block.style.backgroundSize = `${w}px  ${h}px`;
+        }
+      };
+
+      const setAspectRatioOnLoad = () => {
+        const w = parseInt(window.innerWidth, 10);
+        const h = parseInt(window.innerHeight, 10);
+        if (window.innerWidth > 1024) {
+          block.style.backgroundAttachment = 'fixed';
+        }
+        if (window.innerWidth <= 1024) {
+          block.style.backgroundAttachment = 'scroll';
+          return;
+        }
+        block.style.aspectRatio = `${w} / ${h * 0.95}`;
+        block.style.backgroundSize = `${w}px  ${h * 0.95}px`;
+      };
+
+      setAspectRatioOnLoad();
+      window.addEventListener('resize', setAspectRatioOnResize);
+    }
+  }
 }
